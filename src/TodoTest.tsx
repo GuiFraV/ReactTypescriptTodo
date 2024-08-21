d9bf3eaa6c5f2d04407c6cc96c1aeac92859939f
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-21 18:19:19 +0200

    Ok

--- src/TodoTest.tsx
+++ src/TodoTest.tsx
@@ -1,34 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const TodoTest: React.FC = () => {
+const TodoTest = () => {
   interface Todo {
     id: number;
     text: string;
     completed: boolean;
   }
 
-  const [todoText, setTodoText] = useState<string>("");
+  const [todoInput, setTodoInput] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTodoText(e.target.value);
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoInput(e.target.value);
   };
 
-  const addTodo = () => {
+  const handleTodo = () => {
     setTodos([
       ...todos,
-      { id: todos.length + 1, text: todoText, completed: false },
+      { id: todos.length + 1, text: todoInput, completed: false },
     ]);
-    setTodoText("");
+    setTodoInput("");
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   return (
     <div>
-      <input onChange={(e) => handleTodo(e)} value={todoText}></input>
-      <button onClick={addTodo}>Valider</button>
+      <input onChange={(e) => handleInput(e)} value={todoInput}></input>
+      <button onClick={handleTodo}>Valider</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>

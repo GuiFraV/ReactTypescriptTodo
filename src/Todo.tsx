050b20789dcdb2176dd40e2a1f89221dd08cfe1f
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-22 12:48:09 +0200

    oK

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,50 +1,58 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-const Todo = () => {
-  interface Todo {
-    id: number;
-    task: string;
-    completed: boolean;
-  }
+interface Todo {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
+const TodoList: React.FC = () => {
   const [todoValue, setTodoValue] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleText = (e) => {
+  const handleText = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodoValue(e.target.value);
   };
 
   const handleTodo = () => {
-    setTodos([...todos, { id: Date.now(), task: todoValue, completed: false }]);
-    setTodoValue("");
+    if (todoValue.trim() !== "") {
+      setTodos([
+        ...todos,
+        { id: Date.now(), task: todoValue, completed: false },
+      ]);
+      setTodoValue("");
+    }
   };
 
-  const deleteTodo = (e, id: number) => {
+  const deleteTodo = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
     e.stopPropagation();
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
   const isCompleted = (id: number) => {
     setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
     );
   };
 
   return (
     <div>
-      <input onChange={(e) => handleText(e)} value={todoValue}></input>
-      <button onClick={handleTodo}>Valider</button>
+      <input onChange={handleText} value={todoValue} />
+      <button onClick={handleTodo}>Ajouter</button>
       <ul>
         {todos.map((todo) => (
           <li
-            onClick={() => isCompleted(todo.id)}
             key={todo.id}
+            onClick={() => isCompleted(todo.id)}
             style={{ textDecoration: todo.completed ? "line-through" : "none" }}
           >
             {todo.task}
-            <button onClick={(e) => deleteTodo(e, todo.id)}>Delete</button>
+            <button onClick={(e) => deleteTodo(e, todo.id)}>Supprimer</button>
           </li>
         ))}
       </ul>
@@ -52,4 +60,4 @@ const Todo = () => {
   );
 };
 
-export default Todo;
+export default TodoList;
